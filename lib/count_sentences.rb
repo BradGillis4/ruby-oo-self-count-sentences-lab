3af97dabe97f0521665f269aa0390da8dd16f527
BradGillis4 <bradgillis4@gmail.com>
2020-12-17 15:26:50 -0500

    commit tree 8f87c6d7ab02aedb5f74c79b09d2588a13b29dd8

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    count = 0
+    symbol = ["." , "?", "!"]
+    count += 1 if symbol.include?(self[-1])
+    self.each_char.with_index {|char, i| 
+      count += 1 if symbol.include?(char) && self[i+1] == " " 
+    }
+    count
 
   end
 end
